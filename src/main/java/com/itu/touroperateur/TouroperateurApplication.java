8d336d57b9c1f36285405f716c11ae46b04d53ab
Randriamifidisoa Rotsy Finaritra <rotsyfinaritra0@gmail.com>
2026-02-01 20:59:27 +0300

    added app skeleton

---
+++ src/main/java/com/itu/touroperateur/TouroperateurApplication.java
@@ -0,0 +1,13 @@
+package com.itu.touroperateur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouroperateurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouroperateurApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itu/touroperateur/TouroperateurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itu.touroperateur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouroperateurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
